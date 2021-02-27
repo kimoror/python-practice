873a7584aa43b9b32d334774babe830505e035e8
Danila <kimoror@mail.ru>
2021-02-28 00:42:25 +0300

    change loops.Now loop from subset is primary and loop from plenty is nested

--- practice2/Task2_1.py
+++ practice2/Task2_1.py
@@ -1,5 +1,5 @@
 x2_2 = {'cuda': 2, 'eq': 1, 'dm': 0}
-x3 = {'dm': 5, 'arc': 4, 'haxe': 3}
+x3 = {'haxe': 3, 'arc': 4, 'dm': 5}
 x4 = {'urweb': x3, 'gdb': x2_2}
 x0 = {2001: 6, 2020: x4}
 x2_1 = {'cuda': 9, 'eq': 8, 'dm': 7}
@@ -7,8 +7,8 @@ x1 = {'swift': 10, 'antlr': x2_1, 'asp': x0}
 
 
 def level_two(plenty, subset):
-    for el in plenty:
-        for j in subset.keys():
+    for j in subset.keys():
+        for el in plenty:
             if j == el:
                 if isinstance(subset.get(j), int):
                     return subset.get(j)
@@ -21,4 +21,4 @@ def f21(plenty):
     return level_two(plenty, x1)
 
 
-print(f21([2020, 'antlr', 'dm', 'arc', 'urweb']))
+print(f21([2020, 'asp', 'dm', 'dm', 'gdb']))
