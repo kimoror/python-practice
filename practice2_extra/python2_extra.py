bc39b7dd0685af2f1932c39b1df7faadbf67985b
Danila <kimoror@mail.ru>
2021-03-29 00:21:42 +0300

    add converter task in practice2_extra

--- practice2_extra/python2_extra.py
+++ practice2_extra/python2_extra.py
@@ -1,6 +1,12 @@
+import json
+import csv
+import json2html
+
 """
 Преобразовать элементы списка s из строковой в числовую форму.
 """
+
+
 def str_to_int(s):
     return [int(it) for it in s]
 
@@ -8,6 +14,8 @@ def str_to_int(s):
 """
 Подсчитать количество различных элементов в последовательности s.
 """
+
+
 def calc_diff_elem(s):
     return len(set(s))
 
@@ -15,6 +23,8 @@ def calc_diff_elem(s):
 """
 Обратить последовательность s без использования функций.
 """
+
+
 def revert(s):
     return s[::-1]
 
@@ -22,6 +32,8 @@ def revert(s):
 """
 Выдать список индексов, на которых найден элемент x в последовательности s.
 """
+
+
 def index_list(x, s):
     return [i for i in range(len(s)) if x == s[i]]
 
@@ -29,14 +41,20 @@ def index_list(x, s):
 """
 Сложить элементы списка s с четными индексами.
 """
+
+
 def sum_even(s):
     return sum(s[1::2])
+
 
 """
 Найти строку максимальной длины в списке строк s.
 """
+
+
 def max_len(s):
     return max([len(str) for str in s])
+
 
 """
 Тесты
@@ -47,3 +65,41 @@ assert revert([1, 2, 3, 4, 5]) == [5, 4, 3, 2, 1]
 assert index_list(5, [45, 5, 1, 5, 9, 4, 5]) == [1, 3, 6]
 assert sum_even([1, 2, 3, 4]) == 6
 assert max_len(["ads", "dddd", "d", "ddddd"]) == 5
+
+"""
+i = 0
+['much','code','wow'][i] # 24 символа
+Out[1]:
+'much'
+Сократите код в строке №2 до 19 символов без использования функций.
+"""
+# i = 0
+# print(['much'|'code'|'wow'])
+
+
+'''
+Реализовать утилиту командной строки для преобразования данных с сайта с результатами
+проверки робота kispython в форматы csv, ascii и html. Использовать данные вашей группы из table.json.
+'''
+with open('table.json') as file_json:
+    data = json.load(file_json)
+json_data = data['data']
+
+# to_csv
+csv_file = open('file_csv.csv', 'w')
+csv_writer = csv.writer(csv_file)
+count = 0
+for i in json_data:
+    csv_writer.writerow(i)
+
+# to ascii
+ascii_writer = open('ascii.txt', 'w')
+info = json.dumps(json_data, ensure_ascii=False)
+ascii_writer.write(info)
+
+# to html
+html_writer = open('html.html', 'w')
+html_writer.write(json2html.json2html.convert(info))
+
+ascii_writer.close()
+csv_file.close()
